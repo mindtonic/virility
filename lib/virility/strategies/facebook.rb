ecb6f85f4676bae6406f2fecec7d58ec953922f9
Jay Sanders <mindtonic@gmail.com>
2012-10-30 15:39:30 -0400

    Bulletproof all of the Facebook Methods

--- lib/virility/strategies/facebook.rb
+++ lib/virility/strategies/facebook.rb
@@ -2,13 +2,57 @@ module Virility
 	class Facebook < Context
 		BASE_URL = "https://api.facebook.com/method/fql.query?query=SELECT+share_count%2C+like_count%2C+comment_count%2C+total_count%2C+commentsbox_count%2C+click_count+FROM+link_stat+WHERE+url%3D"
 
+		#
+		# Interface Methods
+		#
+
 		def poll
 			@response = self.class.get("#{BASE_URL}%22#{@url}%22")
-			@results = @response.parsed_response["fql_query_response"]["link_stat"]
+
+			if valid_response_test
+				@results = @response.parsed_response["fql_query_response"]["link_stat"]
+			else
+				@results["total_count"] = 0
+			end
 		end
-		
+
 		def count
 			results["total_count"] || 0
 		end
+
+		#
+		# Facebook Attributes
+		#
+
+		def like_count
+			result_exists?(:like_count) ? get_result(:like_count) : 0
+		end
+
+		def click_count
+			result_exists?(:click_count) ? get_result(:click_count) : 0
+		end
+
+		def share_count
+			result_exists?(:share_count) ? get_result(:share_count) : 0
+		end
+
+		def comment_count
+			result_exists?(:comment_count) ? get_result(:comment_count) : 0
+		end
+
+		def commentsbox_count
+			result_exists?(:commentsbox_count) ? get_result(:commentsbox_count) : 0
+		end
+
+		def total_count
+			result_exists?(:total_count) ? get_result(:total_count) : 0
+		end
+		
+	private
+	
+		def valid_response_test
+			@response.respond_to?(:parsed_response) and @response.parsed_response.is_a?(Hash) and !@response.parsed_response["fql_query_response"].nil? and !@response.parsed_response["fql_query_response"]["link_stat"].nil?
+		end
+
 	end
 end
---
+++ spec/strategies/facebook_spec.rb
@@ -0,0 +1,98 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "Virility::Facebook" do
+	before(:each) do
+		@url = "http://creativeallies.com"
+	end
+	
+	share_examples_for "no facebook results" do
+		it "should not raise an error" do
+			lambda { @virility.poll }.should_not raise_error
+		end
+
+		["like_count", "click_count", "share_count", "comment_count", "commentsbox_count", "total_count"].each do |attribute|
+			it "should return 0 for #{attribute}" do
+				@virility.send(attribute.to_sym).should == 0
+			end
+		end	
+	end
+
+	describe "poll" do
+		context "when there is not a valid result" do
+			before(:each) do
+				response = double("HTTParty::Response", :parsed_response => {"fql_query_response"=>{"list"=>"true"}})
+				Virility::Facebook.stub(:get).and_return(response)
+				@virility = Virility::Facebook.new(@url)
+			end
+
+			it_should_behave_like "no facebook results"
+		end
+
+		context "when there is no result" do
+			before(:each) do
+				response = double("HTTParty::Response")
+				Virility::Facebook.stub(:get).and_return(response)
+				@virility = Virility::Facebook.new(@url)
+			end
+
+			it_should_behave_like "no facebook results"
+		end
+
+		context "when there is a result but no fql_query_response" do
+			before(:each) do
+				response = double("HTTParty::Response", :parsed_response => {})
+				Virility::Facebook.stub(:get).and_return(response)
+				@virility = Virility::Facebook.new(@url)
+			end
+
+			it_should_behave_like "no facebook results"
+		end
+
+		context "when there is a result but parsed_response is weird" do
+			before(:each) do
+				response = double("HTTParty::Response", :parsed_response => Object.new)
+				Virility::Facebook.stub(:get).and_return(response)
+				@virility = Virility::Facebook.new(@url)
+			end
+
+			it_should_behave_like "no facebook results"
+		end
+
+		context "when there is a valid result" do
+			before(:each) do
+				response = double("HTTParty::Response", :parsed_response => {"fql_query_response"=>{"list"=>"true", "link_stat"=>{"like_count"=>"977662", "click_count"=>"265614", "share_count"=>"3020040", "comment_count"=>"1118601", "commentsbox_count"=>"0", "total_count"=>"5116303"}}})
+				Virility::Facebook.stub(:get).and_return(response)
+				@virility = Virility::Facebook.new(@url)
+			end
+
+			it "should not raise an error" do
+				lambda { @virility.poll }.should_not raise_error
+			end
+
+			{"like_count"=>"977662", "click_count"=>"265614", "share_count"=>"3020040", "comment_count"=>"1118601", "commentsbox_count"=>"0", "total_count"=>"5116303"}.each do |key, value|
+				it "should return #{value} for #{key}" do
+					@virility.send(key.to_sym).should == value
+				end
+			end
+		end
+
+		context "when there is a valid result, but not all fields are present" do
+			before(:each) do
+				response = double("HTTParty::Response", :parsed_response => {"fql_query_response"=>{"list"=>"true", "link_stat"=>{"like_count"=>"977662", "comment_count"=>"1118601", "commentsbox_count"=>"0", "total_count"=>"5116303"}}})
+				Virility::Facebook.stub(:get).and_return(response)
+				@virility = Virility::Facebook.new(@url)
+			end
+
+			it "should not raise an error" do
+				lambda { @virility.poll }.should_not raise_error
+			end
+
+			{"like_count"=>"977662", "click_count"=>0, "share_count"=>0, "comment_count"=>"1118601", "commentsbox_count"=>"0", "total_count"=>"5116303"}.each do |key, value|
+				it "should return #{value} for #{key}" do
+					@virility.send(key.to_sym).should == value
+				end
+			end
+		end
+	end
+
+end
