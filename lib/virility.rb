d3d129fc1ccb715ad96b74dcceb7d52d69808ebf
Jay Sanders <mindtonic@gmail.com>
2012-10-31 11:24:11 -0400

    Virility Public API methods

--- lib/virility.rb
+++ lib/virility.rb
@@ -5,9 +5,39 @@ require 'virility/version'
 require 'virility/supporter'
 require 'virility/excitation'
 require 'virility/context'
+require 'virility/exceptions'
 
 Dir["#{File.dirname(__FILE__)}/virility/strategies/**/*.rb"].each {|f| require f}
 
 module Virility
-	class UnknownStrategy < StandardError; end
+
+	#
+	# Public API
+	#
+	
+	def self.counts(url)
+		Virility::Excitation.new(url).counts
+	end
+	
+	def self.total(url)
+		Virility::Excitation.new(url).total
+	end
+	
+	def self.poll(url)
+		Virility::Excitation.new(url).poll
+	end
+	
+	def self.object(url)
+		virility = Virility::Excitation.new(url)
+		virility.poll
+		virility
+	end
+
+	#
+	# Factory
+	#
+	
+	def self.factory(strategy, url)
+		Virility::Excitation.new(url).send(strategy)
+	end
 end
