bfc0903c059ce02588445a93b84b934f47fff774
Jay Sanders <mindtonic@gmail.com>
2012-10-31 12:15:49 -0400

    Convert Facebook Strategy to new API

--- lib/virility/strategies/facebook.rb
+++ lib/virility/strategies/facebook.rb
@@ -2,9 +2,12 @@ module Virility
   class Facebook < Strategy
     BASE_URL = "https://api.facebook.com/method/fql.query?query=SELECT+share_count%2C+like_count%2C+comment_count%2C+total_count%2C+commentsbox_count%2C+click_count+FROM+link_stat+WHERE+url%3D"
 
-    def poll
-      @response = self.class.get("#{BASE_URL}%22#{@url}%22")
-      @results = valid_response_test ? @response.parsed_response["fql_query_response"]["link_stat"] : {"total_count" => 0}
+    def census
+      self.class.get("#{BASE_URL}%22#{@url}%22")
+    end
+    
+    def outcome
+			@response.parsed_response["fql_query_response"]["link_stat"]
     end
 
     def count
