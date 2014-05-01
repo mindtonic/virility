ecacbc6696d1580da7ee1f0e011acd911a77254d
Jay Sanders <jay@allieslabs.com>
2014-05-01 14:48:22 -0400

    Adapt to new Delicious Results

--- lib/virility/strategies/delicious.rb
+++ lib/virility/strategies/delicious.rb
@@ -1,6 +1,12 @@
 module Virility
   class Delicious < Strategy
 
+    parser(
+      Proc.new do |body, format|
+        MultiJson.decode(body.scan(/(\{.+\})/).flatten.first)
+      end
+    )
+
     def census
       self.class.get("http://feeds.delicious.com/v2/json/urlinfo/data?url=#{@url}")
     end
