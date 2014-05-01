c8f6423b19e2cb8a31c88a37ba10313f54976164
Jay Sanders <jay@allieslabs.com>
2014-05-01 14:48:13 -0400

    Spacing

--- lib/virility/strategies/stumble_upon.rb
+++ lib/virility/strategies/stumble_upon.rb
@@ -6,14 +6,14 @@ module Virility
         MultiJson.decode(body)["result"]
       end
     )
-  
+
     def census
       self.class.get("http://www.stumbleupon.com/services/1.01/badge.getinfo?url=#{@url}")
     end
-    
+
     def count
       results["views"] || 0
     end
-  
+
   end
 end
