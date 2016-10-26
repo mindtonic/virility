0212d2f4914478f2a0b44a3f7076100493fdc166
Peter Cunning <peter.cunning@storyful.com>
2016-10-26 11:14:47 +0100

    Change Facebook API call

--- lib/virility/strategies/facebook.rb
+++ lib/virility/strategies/facebook.rb
@@ -1,23 +1,45 @@
 module Virility
   class Facebook < Strategy
-    BASE_URL = "https://api.facebook.com/method/links.getStats?urls="
+    # BASE_URL = "https://api.facebook.com/method/links.getStats?urls="
+    BASE_URL = 'https://graph.facebook.com/?fields=share,og_object{engagement,title}&id='.freeze
+
     def census
       self.class.get("#{BASE_URL}#{@url}")
     end
 
     def outcome
-      @response.parsed_response["links_getStats_response"]["link_stat"]
+      response = @response.parsed_response.dig('share')
+      engagement = @response.parsed_response.dig('og_object', 'engagement')
+      response['engagement_count'] = engagement.dig('count')
+      response['social_sentence'] = engagement.dig('social_sentence')
+      response
     end
 
     def count
-      results["total_count"] || 0
+      results.dig('og_object', 'engagement', 'count') || 0
     end
 
   private
 
     def valid_response_test
-      @response.respond_to?(:parsed_response) and @response.parsed_response.is_a?(Hash) and !@response.parsed_response["links_getStats_response"].nil? and !@response.parsed_response["links_getStats_response"]["link_stat"].nil?
+      @response.respond_to?(:parsed_response) && @response.parsed_response.is_a?(Hash) && !@response.parsed_response['share'].nil?
     end
 
   end
 end
+
+# {
+# share: {
+# comment_count: 4,
+# share_count: 97173
+# },
+# og_object: {
+# engagement: {
+# count: 97384,
+# social_sentence: "97K people like this."
+# },
+# title: "Guardians of the Galaxy (2014)",
+# id: "10150298925420108"
+# },
+# id: "http://www.imdb.com/title/tt2015381/"
+# }
